3f6584dd49a5f30a375268391e4f6ccec7e5a275
Shyam Bihari Yadav <shyambihariy390@gmail.com>
2026-08-19 23:34:35 +0530

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>>ans; // this is our answer array.
+
+    void backtrack(vector<int>&nums, int ind, int n, vector<int>v)
+    {
+        if(ind >= n)
+        {
+            ans.push_back(v);
+            return;
+        }
+        v.push_back(nums[ind]); // pick
+        backtrack(nums, ind+1, n, v);
+        v.pop_back(); // not pick
+        backtrack(nums, ind+1, n, v);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        // we need to find all the subsets of the given data.
+        // we will be using backtracking for this.
+        // 1,2,3 - [1,2,3] , [1,2] ,[1,3] , [2,3], [3], [1], [2], []
+
+        // we will use pick ans non pick here.
+
+        // very easy question.
+
+        int n = nums.size();
+        //  {nums, index, n, currsubset-null}
+         backtrack(nums, 0,  n, {}); //function for backtracking/exploring all the paths.
+
+         // once the backtrack returns, we will be having all the subsets
+
+         return ans;
+
+    }
+};
